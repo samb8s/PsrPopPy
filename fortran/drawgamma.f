b03c0fdaa84bb27dc7d55dfa2e4dd73b14000c54
Sam Bates <sam.d.bates@googlemail.com>
2012-10-29 09:25:30 -0400

    Changed FORTRAN refs to 'gamma' to 'gammafunc'
    'gamma' is a built-in function in later fortran versions, causing
    problems with some compilers.

--- fortran/drawgamma.f
+++ fortran/drawgamma.f
@@ -8,20 +8,20 @@ c            where a and m are free parameters
 c            drawgamma uses the rejection method (see Press et al) to
 c            find the appropriate deviate given a choice of a and m
 c
-      real p,gamma,pmin,pmax,a,m,gmax,pgmax,x,y,psrran
+      real p,gammafunc,pmin,pmax,a,m,gmax,pgmax,x,y,psrran
       integer seed
 c
 c     figure out the size of a box which encompasses the gamma distribution
 c
       pgmax=(a-1.0)*m        ! this is the peak of the PDF
-      gmax=gamma(pgmax,a,m)  ! this is the maximum y value of our box
+      gmax=gammafunc(pgmax,a,m)  ! this is the maximum y value of our box
 c
 c     draw uniform deviates from box until one lies under the gamma curve
 c
       do while (.true.)
          x=pmin+(pmax-pmin)*psrran(seed)
          y=gmax*psrran(seed)
-         if (y.le.gamma(x,a,m)) then
+         if (y.le.gammafunc(x,a,m)) then
             drawgamma=x
             return
          endif
