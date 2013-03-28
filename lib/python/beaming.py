7697cd1ec92acba57f53647e3a0c0a866764e4dd
Sam Bates <sam.d.bates@googlemail.com>
2013-03-28 16:24:12 -0400

    Refactored evolve

--- lib/python/beaming.py
+++ lib/python/beaming.py
@@ -3,18 +3,10 @@
 import math
 import random
 
-class Beaming:
+def tm98_fraction(period_s):
     """
-    Various beaming models
+    Tauris and Manchester 1998 beaming fraction
     """
+    periodterm = math.log10(period_s) - 1.0
 
-    def __init__(self):
-        pass
-
-    def tm98_fraction(self, period_s):
-        """
-        Tauris and Manchester 1998 beaming fraction
-        """
-        periodterm = math.log10(period_s) - 1.0
-
-        return 0.03 + 0.09 * periodterm**2.0
+    return 0.03 + 0.09 * periodterm**2.0
